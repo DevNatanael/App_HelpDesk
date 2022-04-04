54c113d880b7c4d983f427479b13bd455cb92a6c
Natanael Otaviano <102233888+DevNatanael@users.noreply.github.com>
2022-04-04 08:15:05 -0300

    Add files via upload

---
+++ registra_chamado.php
@@ -0,0 +1,26 @@
+<?php 
+session_start();
+
+//montagem do testo
+$titulo = str_replace('#', '-', $_POST['titulo']);
+$categoria = str_replace('#', '-',$_POST['categoria']);
+$descricao = str_replace('#', '-', $_POST['descricao']);
+
+$texto =  $_SESSION['id'] . '#' . $titulo . ' # ' .$categoria . ' # '. $descricao . PHP_EOL;
+
+//abrindo o arquivo
+$arquivo= fopen('arquivo.hd' , 'a');
+
+//escrevendo o texto
+fwrite($arquivo, $texto); 
+
+//fechando o arquivo
+fclose($arquivo);
+
+//echo $texto;
+header('Location: abrir_chamado.php');
+
+
+
+
+?>
